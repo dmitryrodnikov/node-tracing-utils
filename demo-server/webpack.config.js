aa328ee00e38ad0061d8bc812ceeaeafc79da149
rodnikovdi <rodnikovdi@vtblife.ru>
2020-05-05 17:03:59 +0300

    Rearrange webpack config

--- demo-server/webpack.config.js
+++ demo-server/webpack.config.js
@@ -1,19 +1,16 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
+const OUTPUT_DIR = 'build';
+
 module.exports = {
+    target: 'node',
+    mode: 'development',
     entry: './src/main.ts',
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, OUTPUT_DIR),
     },
-    target: 'node',
-    mode: 'development',
-    externals: [
-        nodeExternals({
-            whitelist: ['webpack/hot/poll?100'],
-        }),
-    ],
     module: {
         rules: [
             {
@@ -26,4 +23,9 @@ module.exports = {
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
     },
+    externals: [
+        nodeExternals({
+            whitelist: ['webpack/hot/poll?100'],
+        }),
+    ],
 };
