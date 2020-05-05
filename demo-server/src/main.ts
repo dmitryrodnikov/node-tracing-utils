1f2cf017afc51f127d292a3d74d959cf81c3de44
rodnikovdi <rodnikovdi@vtblife.ru>
2020-05-05 20:22:27 +0300

    Use namespace middleware in demo server

--- demo-server/src/main.ts
+++ demo-server/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app-module';
+import { getNamespaceMiddleware } from '../../src/namespace-middleware';
+import { createNamespace } from 'cls-hooked';
+
+const projectNamespace = createNamespace('project-namespace');
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
+    app.use(getNamespaceMiddleware(projectNamespace));
 }
+
 bootstrap();
