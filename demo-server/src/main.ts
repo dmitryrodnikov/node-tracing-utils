fb2ad11324e675810e08da4a75ffc929a4a7ffe4
rodnikovdi <rodnikovdi@vtblife.ru>
2020-05-09 13:00:15 +0300

    Add NestTracingInterceptor

--- demo-server/src/main.ts
+++ demo-server/src/main.ts
@@ -2,13 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app-module';
 import { getNamespaceMiddleware } from '../../lib/src/namespace-middleware';
 import { createNamespace } from 'cls-hooked';
+import {NestTracingInterceptor} from '../../lib/src/nest/nest-interceptor';
+import {initTracer} from 'jaeger-client';
 
 const projectNamespace = createNamespace('project-namespace');
+const tracingConfig = {
+    serviceName: 'items-service',
+    reporter: {
+        collectorEndpoint: '', // todo
+    }
+};
+const tracingOptions = {};
+const tracer = initTracer(tracingConfig, tracingOptions);
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
     app.use(getNamespaceMiddleware(projectNamespace));
+    app.useGlobalInterceptors(new NestTracingInterceptor(projectNamespace, tracer));
 }
 
 bootstrap();
